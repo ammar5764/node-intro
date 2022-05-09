eac7731d18b6afb9047c98deb6bc55302ebe50b0
daniel ammar <d089798228@gmail.com>
2022-05-09 20:10:51 +0300

    intro-node

---
+++ hello.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+//process.stdin.on('data', (data) => {
+  //  const name = data.toString().trim().toUpperCase();
+   // if (name !== '') {
+      fs.writeFileSync(`./notes.txt`, `My name is Daniel`);
+  //  }
+   // else {
+    //    process.stderr.write('input was empty')
+ //   }
+//});
+fs.appendFileSync(`./notes.txt`, ` \nMy name is Rivka`);
+
+// faire la meme chose mais cette fois il doit lire dans la console avec un callback (page 24 du pdf) au lieu de l'ecrire dans le fichier
